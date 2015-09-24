82bee6e0b8c2fecf5284df5f75fc5e1b9c706eac
Artyom Beilis <artyomtnk@yahoo.com>
2015-09-24 23:34:29 +0300

    Updated build and test procedure

--- standalone/run_convert_and_build.sh
+++ standalone/run_convert_and_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export WINEPATH=/usr/lib/gcc/i686-w64-mingw32/4.6
+export WINEPATH="/usr/i686-w64-mingw32/lib;/usr/lib/gcc/i686-w64-mingw32/4.8"
 
 rm -fr /tmp/nwlin /tmp/nw
 
 pushd .
-./convert && mkdir nowide/build && cd nowide/build && cmake -DCMAKE_TOOLCHAIN_FILE=../../../../cppcms_trunk/MinGW.cmake -DCMAKE_INSTALL_PREFIX=/tmp/nw -DRUN_WITH_WINE=ON .. && make && make test && make install
+./convert && mkdir nowide/build && cd nowide/build && cmake -DCMAKE_TOOLCHAIN_FILE=../../MinGW.cmake -DCMAKE_INSTALL_PREFIX=/tmp/nw -DRUN_WITH_WINE=ON .. && make && make test && make install
 popd 
 
 pushd .
