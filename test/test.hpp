10d43c46cd57d3c91d1d3553f3918ae8684a2abc
Alexander Grund <Flamefire@users.noreply.github.com>
2019-12-01 20:58:15 +0100

    Create test_failed function for easier debugging
    Can set breakpoints inside

--- test/test.hpp
+++ test/test.hpp
@@ -1,5 +1,6 @@
 //
 //  Copyright (c) 2012 Artyom Beilis (Tonkikh)
+//  Copyright (c) 2019 Alexander Grund
 //
 //  Distributed under the Boost Software License, Version 1.0. (See
 //  accompanying file LICENSE_1_0.txt or copy at
@@ -11,6 +12,12 @@
 #include <sstream>
 #include <stdexcept>
 
+/// Function called when a test failed to be able set a breakpoint for debugging
+inline void test_failed(const std::string &msg)
+{
+    throw std::runtime_error(msg);
+}
+
 #define TEST(x)                                                                         \
     do                                                                                  \
     {                                                                                   \
@@ -18,7 +25,7 @@
             break;                                                                      \
         std::ostringstream ss;                                                          \
         ss << "Error " #x " in " << __FILE__ << ':' << __LINE__ << " " << __FUNCTION__; \
-        throw std::runtime_error(ss.str());                                             \
+        test_failed(ss.str());                                                          \
     } while(0)
 
 #endif // #ifndef BOOST_NOWIDE_LIB_TEST_H_INCLUDED
