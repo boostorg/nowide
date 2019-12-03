92ca903c1811c829567c776e403c07f3ef5c5473
Peter Dimov <pdimov@gmail.com>
2019-12-03 15:49:37 +0200

    Add copyright and license

--- test/test_iostream.cpp
+++ test/test_iostream.cpp
@@ -1,3 +1,11 @@
+//
+//  Copyright (c) 2015 Artyom Beilis (Tonkikh)
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include <boost/nowide/iostream.hpp>
 
 #include "test.hpp"
