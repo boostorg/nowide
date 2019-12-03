f84620d1f21cb7571f366370af1b45e3a11f97b9
Peter Dimov <pdimov@gmail.com>
2019-12-03 06:49:53 +0200

    Use a random prefix instead of a temp directory, because Cygwin has separate win32 and posix cwd

--- test/test_fs.cpp
+++ test/test_fs.cpp
@@ -5,6 +5,11 @@
 //  accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
 //
+
+#ifdef _MSC_VER
+# define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <boost/nowide/integration/filesystem.hpp>
 #include <boost/nowide/fstream.hpp>
 #include <boost/nowide/cstdio.hpp>
@@ -14,8 +19,8 @@
 
 #include "test.hpp"
 
-static char const *utf8_name = "\xf0\x9d\x92\x9e-\xD0\xBF\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82-\xE3\x82\x84\xE3\x81\x82.txt";
-static wchar_t const *wide_name = L"\U0001D49E-\u043F\u0440\u0438\u0432\u0435\u0442-\u3084\u3042.txt";
+static char const * utf8_name_ = "\xf0\x9d\x92\x9e-\xD0\xBF\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82-\xE3\x82\x84\xE3\x81\x82.txt";
+static wchar_t const * wide_name_ = L"\U0001D49E-\u043F\u0440\u0438\u0432\u0435\u0442-\u3084\u3042.txt";
 
 int main()
 {
@@ -23,30 +28,31 @@ int main()
 
         boost::nowide::nowide_filesystem();
 
-        TEST(boost::nowide::widen(utf8_name) == wide_name);
-        TEST(boost::nowide::narrow(wide_name) == utf8_name);
+        TEST(boost::nowide::widen(utf8_name_) == wide_name_);
+        TEST(boost::nowide::narrow(wide_name_) == utf8_name_);
 
         namespace fs = boost::filesystem;
 
-        fs::path temp = fs::temp_directory_path() / fs::unique_path( "nowide-test_fs-%%%%-%%%%" );
+        std::string prefix = fs::unique_path( "nowide-test_fs-%%%%-%%%%-" ).string();
 
-        fs::create_directory( temp );
-        fs::current_path( temp );
+        std::string utf8_name = prefix + utf8_name_;
+        std::wstring wide_name = boost::nowide::widen( prefix ) + wide_name_;
 
-        boost::nowide::ofstream f(utf8_name);
+        TEST(boost::nowide::widen(utf8_name) == wide_name);
+        TEST(boost::nowide::narrow(wide_name) == utf8_name);
+
+        boost::nowide::ofstream f(utf8_name.c_str());
         TEST(f);
         f << "Test" << std::endl;
         f.close();
+
         TEST(fs::is_regular_file(wide_name)==true);
         TEST(fs::is_regular_file(utf8_name)==true);
-        boost::nowide::remove(utf8_name);
+
+        boost::nowide::remove(utf8_name.c_str());
+
         TEST(fs::is_regular_file(utf8_name)==false);
         TEST(fs::is_regular_file(wide_name)==false);
-
-        fs::current_path( fs::initial_path() );
-
-        boost::system::error_code ec;
-        fs::remove( temp, ec );
     }
     catch(std::exception const &e) {
         std::cerr << "Failed : " << e.what() << std::endl;
