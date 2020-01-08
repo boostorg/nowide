999893c4e6c2fc16fca621b564d5d3dd7045d2d3
Alexander Grund <Flamefire@users.noreply.github.com>
2020-01-08 10:25:49 +0100

    Remove unused u2w_tests array from test

--- test/test_sets.hpp
+++ test/test_sets.hpp
@@ -35,13 +35,6 @@ utf8_to_wide n2w_tests[] = {{"\xf0\x9d\x92\x9e-\xD0\xBF\xD1\x80\xD0\xB8\xD0\xB2\
                             {"\xE3\x82\xFF\xE3\x81\x82", L"\ufffd\u3042"},
                             {"\xE3\xFF\x84\xE3\x81\x82", L"\ufffd\ufffd\u3042"}};
 
-utf8_to_wide u2w_tests[] = {{"\xf0\x9d\x92\x9e-\xD0\xBF\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82-\xE3\x82\x84\xE3\x81\x82.txt",
-                             L"\U0001D49E-\u043F\u0440\u0438\u0432\u0435\u0442-\u3084\u3042.txt"},
-                            {"\xFF\xD0\xBF\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82", L"\uFFFD\u043F\u0440\u0438\u0432\u0435\u0442"},
-                            {"\xD0\xBF\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82\xFF", L"\u043F\u0440\u0438\u0432\u0435\u0442\uFFFD"},
-                            {"\xE3\x82\xFF\xE3\x81\x82", L"\ufffd\u3042"},
-                            {"\xE3\xFF\x84\xE3\x81\x82", L"\ufffd\ufffd\u3042"}};
-
 wide_to_utf8 w2n_tests_utf16[] = {
   {
     L"\U0001D49E-\u043F\u0440\u0438\u0432\u0435\u0442-\u3084\u3042.txt",
@@ -82,7 +75,7 @@ void run_all(std::wstring (*to_wide)(const std::string&), std::string (*to_narro
         std::cout << "  N2W  " << i << std::endl;
         TEST(to_wide(n2w_tests[i].utf8) == n2w_tests[i].wide);
     }
-    int total = 0;
+    size_t total = 0;
     const wide_to_utf8* ptr = 0;
     if(sizeof(wchar_t) == 2)
     {
@@ -93,7 +86,7 @@ void run_all(std::wstring (*to_wide)(const std::string&), std::string (*to_narro
         ptr = w2n_tests_utf32;
         total = sizeof(w2n_tests_utf32) / sizeof(w2n_tests_utf32[0]);
     }
-    for(int i = 0; i < total; i++)
+    for(size_t i = 0; i < total; i++)
     {
         std::cout << "  W2N  " << i << std::endl;
         TEST(to_narrow(ptr[i].wide) == ptr[i].utf8);
