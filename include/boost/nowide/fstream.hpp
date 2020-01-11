777d8c702f2079981a16293632b77defaa804467
Alexander Grund <Flamefire@users.noreply.github.com>
2020-01-11 14:19:52 +0100

    Reduce boilerplate from basic_*fstream classes
    Add a subclass which initializes basic_filebuf so it can be passed to the stream constructor
    Move functions into this subclass using template params for the default arguments and modifiers

--- include/boost/nowide/fstream.hpp
+++ include/boost/nowide/fstream.hpp
@@ -34,67 +34,66 @@ namespace nowide {
     using std::fstream;
 
 #else
+    /// \cond INTERNAL
+    namespace detail {
+        // clang-format off
+        struct StreamTypeIn
+        {
+            static std::ios_base::openmode mode() { return std::ios_base::in; }
+            static std::ios_base::openmode mode_modifier() { return mode(); }
+        };
+        struct StreamTypeOut
+        {
+            static std::ios_base::openmode mode() { return std::ios_base::out; }
+            static std::ios_base::openmode mode_modifier() { return mode(); }
+        };
+        struct StreamTypeInOut
+        {
+            static std::ios_base::openmode mode() { return std::ios_base::in | std::ios_base::out; }
+            static std::ios_base::openmode mode_modifier() { return std::ios_base::openmode(); }
+        };
+        // clang-format on
+
+        /// CRTP Base class for all basic_*fstream classes
+        /// Contains basic_filebuf instance so its pointer can be used to construct basic_*stream
+        /// Provides common functions to reduce boilerplate code
+        /// \tparam T_StreamType One of StreamType* above.
+        ///         Class used instead of value, because openmode::operator| may not be constexpr
+        template<typename T_Basic_FStream, typename T_StreamType>
+        class fstream_impl;
+    } // namespace detail
+    /// \endcond
+
     ///
     /// \brief Same as std::basic_ifstream<char> but accepts UTF-8 strings under Windows
     ///
     template<typename CharType, typename Traits = std::char_traits<CharType> >
-    class basic_ifstream : public std::basic_istream<CharType, Traits>
+    class basic_ifstream : public detail::fstream_impl<basic_ifstream<CharType, Traits>, detail::StreamTypeIn>,
+                           public std::basic_istream<CharType, Traits>
     {
-    public:
-        typedef basic_filebuf<CharType, Traits> internal_buffer_type;
-        typedef std::basic_istream<CharType, Traits> internal_stream_type;
+        typedef detail::fstream_impl<basic_ifstream, detail::StreamTypeIn> fstream_impl;
+        typedef std::basic_istream<CharType, Traits> stream_base;
 
-        basic_ifstream() : internal_stream_type(NULL)
-        {
-            this->init(&buf_);
-        }
+    public:
+        basic_ifstream() : stream_base(&this->buf_)
+        {}
 
         explicit basic_ifstream(const char* file_name, std::ios_base::openmode mode = std::ios_base::in) :
-            internal_stream_type(NULL)
+            stream_base(&this->buf_)
         {
-            this->init(&buf_);
             open(file_name, mode);
         }
 
         explicit basic_ifstream(const std::string& file_name, std::ios_base::openmode mode = std::ios_base::in) :
-            internal_stream_type(NULL)
+            stream_base(&this->buf_)
         {
-            this->init(&buf_);
             open(file_name, mode);
         }
 
-        void open(const std::string& file_name, std::ios_base::openmode mode = std::ios_base::in)
-        {
-            open(file_name.c_str(), mode);
-        }
-        void open(const char* file_name, std::ios_base::openmode mode = std::ios_base::in)
-        {
-            if(!buf_.open(file_name, mode | std::ios_base::in))
-                this->setstate(std::ios_base::failbit);
-            else
-                this->clear();
-        }
-        bool is_open()
-        {
-            return buf_.is_open();
-        }
-        bool is_open() const
-        {
-            return buf_.is_open();
-        }
-        void close()
-        {
-            if(!buf_.close())
-                this->setstate(std::ios_base::failbit);
-        }
-
-        internal_buffer_type* rdbuf() const
-        {
-            return const_cast<internal_buffer_type*>(&buf_);
-        }
-
-    private:
-        internal_buffer_type buf_;
+        using fstream_impl::open;
+        using fstream_impl::is_open;
+        using fstream_impl::close;
+        using fstream_impl::rdbuf;
     };
 
     ///
@@ -102,60 +101,30 @@ namespace nowide {
     ///
 
     template<typename CharType, typename Traits = std::char_traits<CharType> >
-    class basic_ofstream : public std::basic_ostream<CharType, Traits>
+    class basic_ofstream : public detail::fstream_impl<basic_ofstream<CharType, Traits>, detail::StreamTypeOut>,
+                           public std::basic_ostream<CharType, Traits>
     {
-    public:
-        typedef basic_filebuf<CharType, Traits> internal_buffer_type;
-        typedef std::basic_ostream<CharType, Traits> internal_stream_type;
+        typedef detail::fstream_impl<basic_ofstream, detail::StreamTypeOut> fstream_impl;
+        typedef std::basic_ostream<CharType, Traits> stream_base;
 
-        basic_ofstream() : internal_stream_type(NULL)
-        {
-            this->init(&buf_);
-        }
+    public:
+        basic_ofstream() : stream_base(&this->buf_)
+        {}
         explicit basic_ofstream(const char* file_name, std::ios_base::openmode mode = std::ios_base::out) :
-            internal_stream_type(NULL)
+            stream_base(&this->buf_)
         {
-            this->init(&buf_);
             open(file_name, mode);
         }
         explicit basic_ofstream(const std::string& file_name, std::ios_base::openmode mode = std::ios_base::out) :
-            internal_stream_type(NULL)
+            stream_base(&this->buf_)
         {
-            this->init(&buf_);
             open(file_name, mode);
         }
-        void open(const std::string& file_name, std::ios_base::openmode mode = std::ios_base::out)
-        {
-            open(file_name.c_str(), mode);
-        }
-        void open(const char* file_name, std::ios_base::openmode mode = std::ios_base::out)
-        {
-            if(!buf_.open(file_name, mode | std::ios_base::out))
-                this->setstate(std::ios_base::failbit);
-            else
-                this->clear();
-        }
-        bool is_open()
-        {
-            return buf_.is_open();
-        }
-        bool is_open() const
-        {
-            return buf_.is_open();
-        }
-        void close()
-        {
-            if(!buf_.close())
-                this->setstate(std::ios_base::failbit);
-        }
 
-        internal_buffer_type* rdbuf() const
-        {
-            return const_cast<internal_buffer_type*>(&buf_);
-        }
-
-    private:
-        internal_buffer_type buf_;
+        using fstream_impl::open;
+        using fstream_impl::is_open;
+        using fstream_impl::close;
+        using fstream_impl::rdbuf;
     };
 
 #ifdef BOOST_MSVC
@@ -166,62 +135,32 @@ namespace nowide {
     /// \brief Same as std::basic_fstream<char> but accepts UTF-8 strings under Windows
     ///
     template<typename CharType, typename Traits = std::char_traits<CharType> >
-    class basic_fstream : public std::basic_iostream<CharType, Traits>
+    class basic_fstream : public detail::fstream_impl<basic_fstream<CharType, Traits>, detail::StreamTypeInOut>,
+                          public std::basic_iostream<CharType, Traits>
     {
-    public:
-        typedef basic_filebuf<CharType, Traits> internal_buffer_type;
-        typedef std::basic_iostream<CharType, Traits> internal_stream_type;
+        typedef detail::fstream_impl<basic_fstream, detail::StreamTypeInOut> fstream_impl;
+        typedef std::basic_iostream<CharType, Traits> stream_base;
 
-        basic_fstream() : internal_stream_type(NULL)
-        {
-            this->init(&buf_);
-        }
+    public:
+        basic_fstream() : stream_base(&this->buf_)
+        {}
         explicit basic_fstream(const char* file_name,
                                std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out) :
-            internal_stream_type(NULL)
+            stream_base(&this->buf_)
         {
-            this->init(&buf_);
             open(file_name, mode);
         }
         explicit basic_fstream(const std::string& file_name,
                                std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out) :
-            internal_stream_type(NULL)
+            stream_base(&this->buf_)
         {
-            this->init(&buf_);
             open(file_name, mode);
         }
-        void open(const std::string& file_name, std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out)
-        {
-            open(file_name.c_str(), mode);
-        }
-        void open(const char* file_name, std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out)
-        {
-            if(!buf_.open(file_name, mode))
-                this->setstate(std::ios_base::failbit);
-            else
-                this->clear();
-        }
-        bool is_open()
-        {
-            return buf_.is_open();
-        }
-        bool is_open() const
-        {
-            return buf_.is_open();
-        }
-        void close()
-        {
-            if(!buf_.close())
-                this->setstate(std::ios_base::failbit);
-        }
 
-        internal_buffer_type* rdbuf() const
-        {
-            return const_cast<internal_buffer_type*>(&buf_);
-        }
-
-    private:
-        internal_buffer_type buf_;
+        using fstream_impl::open;
+        using fstream_impl::is_open;
+        using fstream_impl::close;
+        using fstream_impl::rdbuf;
     };
 #ifdef BOOST_MSVC
 #pragma warning(pop)
@@ -244,6 +183,56 @@ namespace nowide {
     ///
     typedef basic_fstream<char> fstream;
 
+    // Implementation
+    namespace detail {
+        template<template<typename, typename> class T_Basic_FStream,
+                 typename CharType,
+                 typename Traits,
+                 typename T_StreamType>
+        class fstream_impl<T_Basic_FStream<CharType, Traits>, T_StreamType>
+        {
+            typedef T_Basic_FStream<CharType, Traits> Underlying;
+            typedef basic_filebuf<CharType, Traits> internal_buffer_type;
+
+            Underlying* underlying()
+            {
+                return static_cast<Underlying*>(this);
+            }
+
+        protected:
+            void open(const std::string& file_name, std::ios_base::openmode mode = T_StreamType::mode())
+            {
+                open(file_name.c_str(), mode);
+            }
+            void open(const char* file_name, std::ios_base::openmode mode = T_StreamType::mode())
+            {
+                if(!buf_.open(file_name, mode | T_StreamType::mode_modifier()))
+                    underlying()->setstate(std::ios_base::failbit);
+                else
+                    underlying()->clear();
+            }
+            bool is_open()
+            {
+                return buf_.is_open();
+            }
+            bool is_open() const
+            {
+                return buf_.is_open();
+            }
+            void close()
+            {
+                if(!buf_.close())
+                    underlying()->setstate(std::ios_base::failbit);
+            }
+
+            internal_buffer_type* rdbuf() const
+            {
+                return const_cast<internal_buffer_type*>(&this->buf_);
+            }
+
+            internal_buffer_type buf_;
+        };
+    } // namespace detail
 #endif
 } // namespace nowide
 } // namespace boost
