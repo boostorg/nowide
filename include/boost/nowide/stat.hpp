d6ebda508f41713bd91620c69e47ff202cce9a6f
Alexander Grund <Flamefire@users.noreply.github.com>
2020-06-16 22:55:24 +0200

    Fix stat64 on MinGW

--- include/boost/nowide/stat.hpp
+++ include/boost/nowide/stat.hpp
@@ -12,6 +12,11 @@
 // Include after sys/types.h
 #include <sys/stat.h>
 
+#if defined(__MINGW32__) && defined(__MSVCRT_VERSION__) && __MSVCRT_VERSION__ < 0x0601
+/// Forward declaration in case MinGW32 is used and __MSVCRT_VERSION__ is defined lower than 6.1
+struct __stat64;
+#endif
+
 namespace boost {
 namespace nowide {
 #if !defined(BOOST_WINDOWS) && !defined(BOOST_NOWIDE_DOXYGEN)
@@ -23,11 +28,11 @@ namespace nowide {
     /// \brief Typedef for the file info structure.
     /// Able to hold 64 bit filesize and timestamps on Windows and usually also on other 64 Bit systems
     /// This allows to write portable code with option LFS support
-    using stat_t = struct ::__stat64;
+    typedef struct ::__stat64 stat_t;
     /// \brief Typedef for the file info structure used in the POSIX stat call
     /// Resolves to `struct _stat` on Windows and `struct stat` otherwise
     /// This allows to write portable code using the default stat function
-    using posix_stat_t = struct ::_stat;
+    typedef struct ::_stat posix_stat_t;
 
     /// \cond INTERNAL
     namespace detail {
